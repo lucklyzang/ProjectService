1eb909d7d94e33a18a719ef46c8eb7bf3e84d3a6
lucklyYong <luckyzangvip@163.com>
2025-05-28 14:48:52 +0800

    添加测试组件

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="loading-hint">
+   <div>双向数据绑定:{{ value666 }}</div>
+   <div><button @click="addValue">点击++</button></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  props: {
+    value666 : {
+      type: Number,
+      default: 0
+    }
+  },
+  model: {
+    prop: 'value666',
+    event: 'updateValue666'
+  },
+  data () {
+    return {
+    };
+  },
+  components: {},
+
+  computed: {},
+
+  mounted () {
+  },
+
+  methods: {
+    addValue () {
+      this.$emit('updateValue666',this.value666+1)
+    }
+  }
+}
+
+</script>
+<style lang='less' scoped>
+@import "../common/stylus/variable.less";
+@import "../common/stylus/modifyUi.less";
+@import "../common/stylus/mixin.less";
+.loading-hint {
+  .init-height();
+}
+</style>
